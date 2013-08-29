8a85960df8594e086fd52c241dab7748e411874a
Eugene Sobolev <sobolevea@ukr.net>
2013-08-29 16:05:56 +0300

    Update active_admin

--- lib/active_admin/views/footer.rb
+++ lib/active_admin/views/footer.rb
@@ -5,7 +5,7 @@ module ActiveAdmin
       def build
         super :id => "footer"
         # powered_by_message
-        para link_to("AppFellas", "http://appfellas.nl/", target: :_blank)
+        # para link_to("AppFellas", "http://appfellas.nl/", target: :_blank)
       end
 
       private
