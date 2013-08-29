0b8a5a752821da0aedb0a1daefccca5e9b6fae42
Eugene Sobolev <sobolevea@ukr.net>
2013-08-29 17:52:34 +0300

    Update active_admin

--- app/assets/javascripts/active_admin/base.js
+++ app/assets/javascripts/active_admin/base.js
@@ -14,4 +14,33 @@ $(document).ready(function(){
     'height':'32px',
     'width':'75.4%',
   });
+
+  // // 1. Listen to change event
+  // $('.file_preview').on('change', function() {
+  //   alert(1);
+  //   target_img = $('img', $(this).next());
+  //   console.log(target_img);
+  //   // 2. Check if it has the FileReader class
+  //   if (!this.files) {
+  //     // 2.1. Old enough to assume a real path
+  //     setPreview(this.value, target_img);
+  //   }
+  //   else {
+  //     // 2.2. Read the file content.
+  //     var reader = new FileReader();
+
+  //     reader.onload = function(e) {
+  //       console.log(e.target.result.result)
+  //       setPreview(e.target.result.result, target_img);
+  //     };
+
+  //     reader.readAsDataURL(this.files[0]);
+  //   }
+  // });
+
+  // function setPreview(url, target) {
+  //   // Do preview things.
+  //   target.attr('src', url);
+  // }
+  
 });
