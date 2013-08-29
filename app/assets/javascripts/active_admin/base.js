fe52b1ade99c9469e3ece2881671158334eac9d7
Eugene Sobolev <sobolevea@ukr.net>
2013-08-29 12:24:59 +0300

    Update active_admin

--- app/assets/javascripts/active_admin/base.js
+++ app/assets/javascripts/active_admin/base.js
@@ -3,3 +3,10 @@
 //= require jquery_ujs
 
 //= require active_admin/application
+
+
+$(document).ready(function(){
+  if($(".alert").size() > 0) {
+    $(".alert").delay("5000").fadeOut();
+  }
+});
